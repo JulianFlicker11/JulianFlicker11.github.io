867e8dd2c53af9445c729197377dd5c5b478e759
JulianFlicker11 <rykowolf11@gmail.com>
2025-07-05 20:03:28 -0400

    Add initial gradebook HTML and JS stubs
    Created JulianFlicker_gradebook.html with a basic table structure and linked gradebook.js. Added stub functions in gradebook.js for fetching and populating grade data.

---
+++ gradebook.js
@@ -0,0 +1,11 @@
+function fetchGradeData() {
+    console.log("Fetching grade data...");
+}
+
+function populateGradebook() {
+    console.log("Populating gradebook...");
+}
+
+// Call the stubs to test
+fetchGradeData();
+populateGradebook();
